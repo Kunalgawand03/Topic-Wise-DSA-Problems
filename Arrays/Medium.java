410ee2e21bf56c019fadc1f45dcf19c979602c64
Kunal Gawand <143700746+Kunalgawand03@users.noreply.github.com>
2026-01-07 10:18:06 +0530

    Update Medium.java

--- Arrays/Medium.java
+++ Arrays/Medium.java
@@ -317,3 +317,61 @@ class Solution {
         return -1;
     }
 }
+
+
+
+
+Kadane's Algorithm : Maximum Subarray Sum in an Array
+// Problem Statement: Given an integer array nums, find the subarray with the largest sum and return the sum of the elements present in that subarray.
+
+// A subarray is a contiguous non-empty sequence of elements within an array.
+
+//Input:
+//  nums = [2, 3, 5, -2, 7, -4]  
+// Output:
+//  15  
+// Explanation:
+//  The subarray from index 0 to index 4 has the largest sum = 15, which is the maximum sum of any contiguous subarray.
+
+//brute
+class Solution {
+    // Function to find maximum sum of subarrays
+    public int maxSubArray(int[] nums) {
+        
+        /* Initialize maximum sum with
+           the smallest possible integer */
+        int maxi = Integer.MIN_VALUE;
+
+        // Iterate over each starting index of subarrays
+        for (int i = 0; i < nums.length; i++) {
+            
+            /* Iterate over each ending index
+               of subarrays starting from i */
+            for (int j = i; j < nums.length; j++) {
+                
+                /* Variable to store the sum
+                   of the current subarray */
+                int sum = 0;
+
+                // Calculate the sum of subarray nums[i...j]
+                for (int k = i; k <= j; k++) {
+                    sum += nums[k];
+                }
+
+                /* Update maxi with the maximum of its current
+                   value and the sum of the current subarray */
+                maxi = Math.max(maxi, sum);
+            }
+        }
+
+        // Return the maximum subarray sum found
+        return maxi;
+    }
+}
+
+
+
+
+
+
+
